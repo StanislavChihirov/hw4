300c2d44dda19600b567d36f816ae94dc14eb843
StanislavChihirov <stanislav.chigirev@gmail.com>
2021-07-29 02:14:59 +0300

    Change in .sh file

--- instapache.sh
+++ instapache.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sudo apt update
 sudo apt install apache2 -y
-echo "<h1>This Web Server run with IP: $myip</h1> <h2>Hello byurkovskiy ! :-)</h2><h3>Check please homework )))))</h3>" > /var/www/html/index.html
+echo "<h1>Hello byurkovskiy ! :-)</h1><h2>Check please homework )))</h2>" > /var/www/html/index.html
 sudo service apache2 start
